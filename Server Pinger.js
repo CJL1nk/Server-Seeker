f1afb022257fa83cbdb978747143c9a4a18588b2
CJ_Link <cmlarsonjr@gmail.com>
2023-10-07 17:59:14 -0700

    Added IP Logger

--- Server Pinger.js
+++ Server Pinger.js
@@ -1,13 +1,16 @@
 const cluster = require('cluster');
 const fs = require('fs');
 const outputFile = 'server_info.json';
+const outputFile2 = 'found_ips.json';
 let mcpinger = require('mcpinger');
+
 
 function generateRandomIP() {
     const getRandomNumber = () => Math.floor(Math.random() * 256);
     const ipParts = Array.from({ length: 4 }, getRandomNumber);
     return ipParts.join('.');
 }
+
 
 function main() {
 
@@ -31,13 +34,25 @@ function main() {
         .catch((error) => {
 
             if (error.message === 'Socket timeout') {
-                console.log(`Could not establish connection to: ${randomIP}`);
+                console.log(`No network path to ${randomIP}`);
+            }
+            else if (error.code === 'ECONNREFUSED' || error.code === 'ENETUNREACH' || error.code === 'EADDRNOTAVAIL') {
+
+                console.log(`Path to ${randomIP} found with no Minecraft server`);
+
+                let existingIPS = []
+                if (fs.existsSync(outputFile2)) {
+                    existingIPS = JSON.parse(fs.readFileSync(outputFile2));
+                }
+                existingIPS.push(randomIP);
+                fs.writeFileSync(outputFile2, JSON.stringify(existingIPS, null, 2));
             }
             else {
                 console.error(error);
             }
         });
 }
+
 
 if (cluster.isMaster) {
 
