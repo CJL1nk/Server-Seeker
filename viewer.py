f05364c4898d7f0c8bddf70bc884faf066e8ec59
KalebSchmidlkofer <kaleb@ashbyte.com>
2023-10-09 13:41:06 -0700

    update: Fixed viewer.py for linux

--- viewer.py
+++ viewer.py
@@ -1,10 +1,12 @@
 import json
 import os
-
+file = os.path.join(os.path.dirname(__file__), 'server_info.json')
 print("Servers Found:")
-with open (f"{os.getcwd()}\\server_info.json") as file:
+  
+with open(file, 'r') as file:
     itemNum = 0
     data = json.load(file)
+    # print(data)
     for item in data:
         print("-" * 25)
         print("IP:", data[itemNum]["ip"])
@@ -12,4 +14,4 @@ with open (f"{os.getcwd()}\\server_info.json") as file:
         print("-" * 25)
         itemNum += 1
 
-os.system("pause")
+input('Press Enter to Continue!')
