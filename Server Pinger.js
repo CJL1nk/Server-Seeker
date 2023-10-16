afed28bb73878e2a42a0b8800573aea3c8257c9b
CJ_Link <cmlarsonjr@gmail.com>
2023-10-16 16:58:08 -0700

    Cleaner Code and documented

--- Server Pinger.js
+++ Server Pinger.js
@@ -5,9 +5,6 @@ let path = require('path');
 
 //########################################################
 
-// SET TO TRUE IF YOU WANT TO LOG ALL IPS FOUND, EVEN IF NO MINECRAFT SERVER RESPONSE
-let logIPS = false;
-
 // FILE AND DIR NAMES
 const mainOutputFile = 'server_info.json';
 const dir = 'Server Data';
@@ -15,110 +12,82 @@ const dir = 'Server Data';
 const intervalTime = 25;
 const maxThreads = 200;
 
-var currentThreads = 0;
-
 //########################################################
-
-// CREATE FILES IF THEY DONT EXIST
-if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-}
-
-if (!fs.existsSync(mainOutputFile)) {
-    fs.writeFileSync((mainOutputFile), '[]');
-}
 
 
 function generateRandomIP() {
-    const getRandomNumber = () => Math.floor(Math.random() * 256);
-    const ipParts = Array.from({ length: 4 }, getRandomNumber);
-    return ipParts.join('.');
+	const getRandomNumber = () => Math.floor(Math.random() * 256);
+	const ipParts = Array.from({ length: 4 }, getRandomNumber);
+	return ipParts.join('.');
 }
 
 
-function main(currentOutputFile) {
-
-    const randomIP = generateRandomIP();
-
-    mcpinger.java({ host: randomIP }).then((res) => {
-
-        // DELETE USELESS DATA AND PUT IP IN
-        delete res.favicon;
-        res.ip = randomIP;
-
-        console.log('Server found!')
-        console.log(res);
-
-        let currentExistingData = [];
-        let totalExistingData = [];
-
-        // CREATE MAIN OUTPUT FILE IF IT DOESNT EXIST
-        if (fs.existsSync(mainOutputFile)) {
-
-            totalExistingData = JSON.parse(fs.readFileSync(mainOutputFile));
-            totalExistingData.push(res);
-        }
-
-        // CREATE CURRENT OUTPUT FILE IF IT DOESNT EXIST IN FOLDER
-        if (fs.existsSync(path.join(dir, currentOutputFile))) {
-
-            currentExistingData = JSON.parse(fs.readFileSync(path.join(dir, currentOutputFile)));
-            console.log(currentExistingData)
-            currentExistingData.push(res);
-        }
-
-        // WRITE TO BOTH FILES
-        fs.writeFileSync(mainOutputFile, JSON.stringify(totalExistingData, null, 2));+
-        fs.writeFileSync(path.join(dir, currentOutputFile), JSON.stringify(currentExistingData, null, 2));
-
-        }).catch((error) => {
-
-            if (error.message === 'Socket timeout') {
-                console.log(`No network path to ${randomIP}`);
-            }
-            else if (error.code === 'ECONNREFUSED' || error.code === 'ENETUNREACH' || error.code === 'EADDRNOTAVAIL' || error.code === 'ECONNRESET') {
-
-                console.log(`Path to ${randomIP} found with no Minecraft server`);
-            }
-            else {
-                console.error(error);
-            }
-        }).finally(() => {
-            process.exit()
-        });
-}
-
-
+// CODE FOR THE MAIN PROCESS
 if (cluster.isMaster) {
 
-    try {
+	try {
 
-        process.env.currentOutputFile = (new Date()).toISOString().replace(/[-T:.]/g, '_').slice(0, -5) + '.json';
+		// TOP 10 READABLE CODE
+		process.env.currentOutputFile = (new Date()).toISOString().replace(/[-T:.]/g, '_').slice(0, -5) + '.json';
+		var currentThreads = 0;
 
-        // also create file if folder doesn't exist
-        if (!fs.existsSync(path.join(dir, process.env.currentOutputFile))) {
-            fs.writeFileSync(path.join(dir, process.env.currentOutputFile), '[]');
-        }
+		// CREATE FILES IF THEY DONT EXIST
+		if (!fs.existsSync(dir)) {fs.mkdirSync(dir)}
+		if (!fs.existsSync(path.join(dir, process.env.currentOutputFile))) {fs.writeFileSync(path.join(dir, process.env.currentOutputFile), '[]')}
+		if (!fs.existsSync(mainOutputFile)) {fs.writeFileSync((mainOutputFile), '[]')}
 
-        cluster.on('exit', (worker, code, signal) => {
-            //console.log(`Worker ${worker.process.pid} exited with code ${code}`);
-            currentThreads--;
-            //console.log(currentThreads);
-        });
+		// DECREMENTS NUMBER OF PROCESSES WHEN IT FINISHES
+		cluster.on('exit', (worker, code, signal) => {
+			currentThreads--;
+		});
 
-        setInterval(() => {
+		// MAKES NEW PROCESS FORK EVERY {intervalTime} MS
+		setInterval(() => {
+			if (currentThreads < maxThreads) {
+				const worker = cluster.fork();
+				currentThreads++;
+			}
+		}, intervalTime);
 
-            if (currentThreads < maxThreads) {
-                const worker = cluster.fork();
-                currentThreads++;
-                console.log('Threads:', currentThreads);
-            }
-        }, intervalTime);
-    }
-    catch(error) {
-        console.log('Unknown error occurred.');
-    }
+		setInterval(() => {console.log('Process Forks:', currentThreads)}, 2000)
+	}
+	catch(error) {console.log(error)}
 }
+
+// CODE FOR THE PROCESS FORKS
 else {
-    main(process.env.currentOutputFile);
+
+	const randomIP = generateRandomIP();
+
+	mcpinger.java({ host: randomIP }).then((res) => {
+
+		// DELETE USELESS DATA AND PUT IP IN
+		delete res.favicon;
+		res.ip = randomIP;
+
+		console.log('Server found!')
+		console.log(res);
+
+		let currentExistingData = [];
+		let totalExistingData = [];
+
+		totalExistingData = JSON.parse(fs.readFileSync(mainOutputFile));
+		totalExistingData.push(res);
+
+		currentExistingData = JSON.parse(fs.readFileSync(path.join(dir, process.env.currentOutputFile)));
+		currentExistingData.push(res);
+
+		// WRITE TO BOTH FILES
+		fs.writeFileSync(mainOutputFile, JSON.stringify(totalExistingData, null, 2));+
+		fs.writeFileSync(path.join(dir, process.env.currentOutputFile), JSON.stringify(currentExistingData, null, 2));
+
+	})
+	.catch((error) => {
+
+		// PRINT IF THE IP SENDS A NON MINECRAFT RESPONSE
+		if (error.message != 'Socket timeout') {
+			console.log(`Path to ${randomIP} found with no Minecraft server`);
+		}
+	})
+	.finally(() => {process.exit()});
 }
